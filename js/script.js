a240852ee5bc7231a0902039af0c2b34e630c613
Sibo_Fulgencio <eduardo.pacheco@utexas.edu>
2022-07-25 03:15:19 -0500

    Main structure & API connection

--- js/script.js
+++ js/script.js
@@ -3,19 +3,21 @@ var appid = '&appid='
 
 var urlCityGeocoding = "http://api.openweathermap.org/geo/1.0/direct?q="
 
+var searchForm = $("#searchForm");
+var searchCity = $('input[name="inputCity"]');
+var searchCountry = $('input[name="inputCountry"]');
+var cityOptions = $("#cityOptions");
 
-var searchCities = $('input[name="searchCities"]');
-var searchCity = $('#searchCity');
+var cityName;
 
-var cityOption;
-
-
+var temp;
 
 
 
 function getCityLocation(cityName){
 
-    var requestUrl = urlCityGeocoding + cityName + "&limit=10" + appid + owappkey;
+    var requestUrl = urlCityGeocoding + cityName + "&limit=10" + appid + config.owappkey;
+    console.log(requestUrl);
 
     fetch(requestUrl)
         .then(function (response) {
@@ -23,32 +25,106 @@ function getCityLocation(cityName){
         }).then(function (data) {
             console.log('City Location INFO\n----------');
             console.log(data);
-            // console.log(data.length);
-
-
-
+            
+            var queryResults = data;
             
 
+            for (let i = 0; i < queryResults.length; i++) {
+                var tableRow = $('<tr>');
+                tableRow.attr('data-lat',queryResults[i].lat)
+                tableRow.attr('data-lon',queryResults[i].lon)                
+                tableRow.append('<th scope="row">' + eval(i+1) + '</th>');
+                tableRow.append('<td >' + queryResults[i].name + '</td>');
+                tableRow.append('<td>' + queryResults[i].state + '</td>');
+                var countryName = countryDB.filter(param => {return param.code == queryResults[i].country});
+                countryName = countryName[0].label;
+                tableRow.append('<td>' + countryName + '</td>');
+                cityOptions.append(tableRow);
+            }
 
-
+            
     });
 }
 
 
-searchCities.keyup(function () {
-    event.stopPropagation();
-    var textQuery = searchCities.val();
 
-    if (textQuery.length > 2) {
-        getCityLocation(textQuery);
+var submitSearch = function (event) {
+    cityOptions.html("");
+    event.preventDefault();
+    
+    var textQuery = searchCity.val();
 
-                
-    }
+    if (searchCountry.val()){
+    var countryCode = countryDB.filter(param => { return param.label == searchCountry.val()})[0].code;};
 
-
+    textQuery += ",," + countryCode;
     console.log(textQuery);
-
+  
+    getCityLocation(textQuery);
 
     
+};
+
+
+// Search City
+searchForm.on('submit', submitSearch);
+
+
+// Select  City Option
+cityOptions.on('click', "tr" , function (event) {
+    var latitude = $(event.target).parent().attr('data-lat');
+    var longitude = $(event.target).parent().attr('data-lon');
+    var cityName = $(event.target).parent().children().eq(1).text();
+    var stateName = $(event.target).parent().children().eq(2).text();
+    var countryName = $(event.target).parent().children().eq(3).text();
+    
+    // var countryName = countryDB.filter(param => {return param.code == $(event.target).parent().children().eq(3).text()});
+    // countryName = countryName[0].label;
+    
+    coord = [latitude, longitude];
+    info = [cityName, stateName, countryName];
+
+    queryWeatherConditions(coord, info);
+
 });
+
+
+function queryWeatherConditions(coord, info) {
+
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + coord[0] + '&lon=' + coord[1] + '&exclude=alerts&units=metric&appid=' + config.owappkey;
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (weatherData) {
+            console.log('XXXXX WEATHER INFO\n----------     --------     ----');
+            console.log(weatherData);       
+
+            // temp = weatherData;
+            if (weatherData){
+                document.getElementById("currentIcon").src = "http://openweathermap.org/img/wn/"+ weatherData.current.weather[0].icon +"@2x.png";
+                
+                var cityText = info[0];
+                if (info[1]) {cityText += ", " + info[1]}
+
+                $("#currentTitle").text(cityText)
+
+                $('#currenTemp').text(weatherData.current.temp + '°C')
+
+
+
+            
+            };
+
+            
+            
+    });
+
+
+
+}
+
+
+
+
 
